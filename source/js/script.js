93bbd6f226970ee72ef27dfe2f5b5b40e5ac976c
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-07-01 19:40:17 -0700

    доработан слайдер с JS

--- source/js/script.js
+++ source/js/script.js
@@ -19,4 +19,12 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + "</span>";
+      },
+    },
+  });
